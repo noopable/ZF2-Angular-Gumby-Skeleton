23d63258fe392dd7bd9607418539f2f48d783962
Tomoaki Kosugi <kipspro@gmail.com>
2013-12-23 22:55:11 +0900

    Local Module draft

---
+++ module/Document/config/invokables.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+            'Document\Controller\Index' => 'Document\Controller\IndexController',
+            'StandardPageController'  => 'Flower\Stdlib\Page\Controller\StandardPageController',
+            'Document\Controller\Error' => 'Document\Controller\ErrorController',
+            'not-found' => 'Document\Controller\NotFoundController',
+);
